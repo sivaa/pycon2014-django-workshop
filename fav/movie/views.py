848a45effa859314121cd4d1de173951b7957352
sivaa <siva@sivaa.in>
2014-09-24 19:55:54 +0530

    Added the movie list in HTML

--- fav/movie/views.py
+++ fav/movie/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 from movie.models import Movie
 
 
+def _get_movies():
+    return Movie.objects.all()
+
 def movies(request):
     if request.method == 'GET':
-        return render(request, "movies.html")
+        return render(request, 
+                      "movies.html",
+                      {"movies"  : _get_movies()})
 
     if request.method == 'POST':
         movie_name = request.POST.get("movie_name")
@@ -14,6 +19,7 @@ def movies(request):
         message = "Movie '{}' is added successfully.".format(movie_name)
         return render(request, 
                       "movies.html",
-                      {"message" : message})
+                      {"message" : message,
+                       "movies"  : _get_movies()})
 
     return ("Invalid Request")
