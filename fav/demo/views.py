6752939aaec8dd54e71f16a16190ea135cd211fb
sivaa <siva@sivaa.in>
2014-09-24 12:39:25 +0530

    Complete Hello World Scenarios

---
+++ fav/demo/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import datetime
+
+def hello1(request):
+	return HttpResponse("Hello World!")
+
+def hello2(request):
+	return HttpResponse("<h1>Hello World!</h1>")
+
+def hello3(request):
+	return HttpResponse("<h1>Hello World! at {} </h1>".format(datetime.now()))
+
+def hello4(request):
+	name = request.GET.get("name", "Django")
+	return HttpResponse("Hello <strong>{}</strong>!".format(name))
+
+def hello5(request):
+	return render(request, "hello5.html")
+
+def hello6(request):
+	return render(request, 
+		      	  "hello6.html",
+		      	   {'current_time' : datetime.now() })
+
+def hello7(request):
+	name = request.GET.get("name", "Django")
+	return render(request, 
+	      	      "hello7.html",
+      	          {'name' : name })
---
+++ fav/fav/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fav.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^hello1/', 'demo.views.hello1'),
+    url(r'^hello2/', 'demo.views.hello2'),
+    url(r'^hello3/', 'demo.views.hello3'),
+    url(r'^hello4/', 'demo.views.hello4'),
+    url(r'^hello5/', 'demo.views.hello5'),
+	url(r'^hello6/', 'demo.views.hello6'),
+	url(r'^hello7/', 'demo.views.hello7'),
+)
